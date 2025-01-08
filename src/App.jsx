1e2e1534890f8d68165a433a8e6ad0f2961f1a0b
Prikshit2803 <prikshit.cs20@rvce.edu.in>
2025-01-08 23:51:36 +0530

    Game over + additional chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard.jsx";
+import GameOver from "./components/GameOver.jsx";
 import Log from "./components/Log.jsx";
 import Player from "./components/Player";
+import { WINNING_COMBINATIONS } from "./winning-combinations.jsx";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns){
 
@@ -19,6 +27,28 @@ function App() {
   // const [activePLayer,setActivePlayer] = useState('X');
   const [gameTurns,setGameTurns] = useState([]);
 
+  let gameBoard = [...initialGameBoard.map(array => [...array])]; //without deep-copy the restart match wouldnt work, as gameboard= initialBoard is a bug as initialBoard is passed by reference and cant be actually reset ...refer lec. 93
+
+    for( const turn of gameTurns){
+        const {square , player} = turn;
+        const {row,col} = square;
+
+        gameBoard[row][col] = player;
+    }
+
+    let winner;
+
+    for(const combination of WINNING_COMBINATIONS){
+      const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+      const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+      const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+      if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+        winner = firstSquareSymbol;
+      }
+    }
+
+    const isDraw = gameTurns.length === 9 && !winner;
   const activePLayer = deriveActivePlayer(gameTurns);
   function handleSelectSquare(rowIndex,colIndex){
     // setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ?'O' : 'X');
@@ -30,11 +60,16 @@ function App() {
       const updatedGameTurns = [{square: {row : rowIndex,col : colIndex}, player : currentPlayer},...prevTurns,];
 
       return updatedGameTurns;
-    })
+    });
 
   }
 
-  return <main>
+  function handleRestart(){
+    setGameTurns([]);
+  }
+
+  return (
+  <main>
     <div id="game-container">
      <ol id="players" className="highlight-player">
      
@@ -42,13 +77,14 @@ function App() {
      <Player initialName="Player 2" symbol="O" isActive={activePLayer==='O'} />
 
      </ol>
-
-     <GameBoard  onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+    {(winner || isDraw) && <GameOver winner = {winner}  onRestart = {handleRestart}/>}
+     <GameBoard  onSelectSquare={handleSelectSquare} board={gameBoard}/>
 
     </div>
 
     <Log turns={gameTurns}/>
   </main>
+  );
 }
 
 export default App
